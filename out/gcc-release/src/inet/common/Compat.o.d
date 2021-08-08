012b2fc9b3c95f0d86ae17974e1f9bb3c7174b36
Ricardo Pagoto Marinho <ricardopmarinho@gmail.com>
2021-08-07 21:14:18 -0300

    routing sixgen

--- out/gcc-release/src/inet/common/Compat.o.d
+++
@@ -1,6 +0,0 @@
-../out/gcc-release/src/inet/common/Compat.o: inet/common/Compat.cc \
- inet/common/Compat.h /home/ricardo/omnetpp-5.6.2/include/omnetpp.h
-
-inet/common/Compat.h:
-
-/home/ricardo/omnetpp-5.6.2/include/omnetpp.h:
